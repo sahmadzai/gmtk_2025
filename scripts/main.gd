1a0b96659cbc2451477324dcfb1cedbf7ee36684
sahmadzai <yusufkarem22@gmail.com>
2025-08-02 02:26:11 -0400

    Add asset files, take 3

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 func _ready():
 	var ui = $inGameUI
-	var player = $Player
-	ui.connect("move_inputs_updated", Callable(player, "_on_move_inputs_updated"))
-	ui.connect("final_move_input_updated", Callable(player, "_on_final_move_input_updated"))
-	player.connect("backspace_pressed", Callable(ui, "_on_backspace"))
-	player.connect("actions_list_cleared", Callable(ui, "_ready"))
+	var ball_player = $BallPlayer
+	ui.connect("move_inputs_updated", Callable(ball_player, "_on_move_inputs_updated"))
+	ui.connect("final_move_input_updated", Callable(ball_player, "_on_final_move_input_updated"))
+	ball_player.connect("backspace_pressed", Callable(ui, "_on_backspace"))
+	ball_player.connect("actions_list_cleared", Callable(ui, "_ready"))
