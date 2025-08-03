9a436274bb02530865897ae4ee864a4bd891d796
sahmadzai <yusufkarem22@gmail.com>
2025-08-03 03:34:13 -0400

    feature(action): update max action count for levels 1,2,3; delete old levels

--- scripts/gameState.gd
+++ scripts/gameState.gd
@@ -4,39 +4,36 @@ var was_win_transition := false
 var next_scene_path := ""
 var shot_count: int = 0
 
-# Dictionary of starting positions for each level
-var level_start_positions := {
+# Dictionary of starting positions for each level0
+var level0_start_positions := {
 	"tutorial01": Vector2(88, 104),
 	"tutorial02": Vector2(136, 40),
 	"tutorial03": Vector2(104, 32),
-	"level1": Vector2(88, 81),
-	"level2": Vector2(104, 177),
-	"level3": Vector2(152, 113),
-	"level4": Vector2(152, 113)
+	"level01": Vector2(104, 88),
+	"level02": Vector2(88, 152),
+	"level03": Vector2(152, 104)
 }
 
 # Dictionary mapping current scene name to next scene path
-var level_progression := {
+var level0_progression := {
 	"tutorial01": "res://scenes/tutorial02.tscn",
 	"tutorial02": "res://scenes/tutorial03.tscn",
-	#"tutorial03": "res://scenes/level1.tscn",
-	"tutorial03": "res://scenes/credits.tscn", # Comment in PROD, just testing end screen
-	"level1": "res://scenes/level2.tscn",
-	"level2": "res://scenes/level3.tscn",
-	"level3": "res://scenes/level4.tscn",
-	"level4": "res://scenes/credits.tscn"
+	"tutorial03": "res://scenes/level01.tscn",
+	#"tutorial03": "res://scenes/credits.tscn", # Comment in PROD, just testing end screen
+	"level01": "res://scenes/level02.tscn",
+	"level02": "res://scenes/level03.tscn",
+	"level03": "res://scenes/credits.tscn"
 }
 
-# Dictionary mapping of how many input‐buttons each level has
+# Dictionary mapping of how many input‐buttons each level0 has
 var max_inputs := {
 	"tutorial01": 1,
 	"tutorial02": 4,
 	"tutorial03": 5,
-	"level1":     6,
-	"level2":     6,
-	"level3":     7,
-	"level4":     8,
+	"level01":    5,
+	"level02":    5,
+	"level03":    3,
 }
 
 func get_next_scene_path(current_scene_name: String) -> String:
-	return level_progression.get(current_scene_name, "res://scenes/tutorial01.tscn")
+	return level0_progression.get(current_scene_name, "res://scenes/tutorial01.tscn")
